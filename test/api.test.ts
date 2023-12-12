d05981d31b6173bb67a72a834a4a08549e345f4f
= <adonisinoda07@gmail.com>
2023-12-12 20:44:13 -0300

    delete tools.json

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,15 +1,63 @@
 const request = require("supertest");
 const validListTools = require("./mocks/valid-list-tools.json")
+const validQueryTool = require("./mocks/valid-query-tool.json")
+import {join} from  "path"
+import {readFileSync,writeFileSync,existsSync} from  "fs"
 import server from "../src/index";
 
 afterEach(() => {
   server.close();
 });
+
+beforeEach(()=>{
+  const srcDataBase = join(__dirname, '..', 'database', 'tools.json')
+  const PathDataBase = join(__dirname, '..', 'database', 'base.json')
+  const database = readFileSync(PathDataBase, 'utf-8')
+    writeFileSync(srcDataBase, database)
+})
+
 describe("API test", () => {
-  describe("/tools:get", () => {
+
+  describe("/tools:GET", () => {
     it("sucesso listando todos", async () => {
       let {_body} = await request(server).get("/tools");
       expect(_body).toEqual(validListTools);
+    });
+  });
+
+  describe("/tools?tag=?:GET", () => {
+    it("sucesso buscando uma ferramenta", async () => {
+      let {_body} = await request(server).get("/tools?tag=json");
+      expect(_body).toEqual(validQueryTool);
+    });
+  });
+
+
+  describe("/tools?tag=?:POST", () => {
+    it("sucesso listando todos", async () => {
+      const payload = {
+        description: "Extremely fast and simple, low-overhead web framework for NodeJS. Supports HTTP2.",
+        link: "https://www.fastify.io/",
+        tags: [
+            "web",
+            "framework",
+            "node",
+            "http2",
+            "https",
+            "localhost"
+        ],
+        title: "fastify"
+    }
+      let {_body} = await request(server).post("/tools?tag=json").send(payload)
+      expect(_body.id).toBeGreaterThanOrEqual(1);
+      expect(_body.id).toBeLessThanOrEqual(1999);
+    });
+  });
+
+  describe("/tools/:id :DELETE", () => {
+    it("sucesso deletar item", async () => {
+      let {_body} = await request(server).delete("/tools/2");
+      expect(_body).toEqual(true);
     });
   });
 });
